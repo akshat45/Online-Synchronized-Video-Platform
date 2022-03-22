54c4ef9bc841daec2e536aaa8aaaf611f5cb86e4
Akshat Gupta <akshatg450@gmail.com>
2022-03-22 16:23:42 +0530

    navbar done and home page modified and some more changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,37 @@
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import Alert from "@mui/material/Alert";
 import Grid from "@mui/material/Grid";
 import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
+import "./homepage.css";
 
 function Home() {
   const location = useLocation();
   const [alert, setAlert] = useState(null);
   const [alerterror, setError] = useState(null);
 
+  useEffect(() => {
+    if (location?.state?.error)
+      setError(location?.state?.message);
+    else
+      setAlert(location?.state?.message);
 
+    const timeId = setTimeout(() => {
+      setAlert(null);
+      setError(null);
+      location.state = null;
+    }, 3500);
+
+    return () => {
+      location.state = null;
+      clearTimeout(timeId);
+    };
+  }, [location.state]);
 
   return (
     <div className="App">
+      <Navbar />
         <div
           style={{
             "background-image": "url('back_image-2.jpg')",
@@ -80,7 +99,7 @@ function Home() {
               <h1
                 style={{
                   "font-family": "'Baloo Bhaijaan 2', cursive",
-                  "font-size": "100px",
+                  "font-size": "70px",
                   lineHeight: '100px'
                 }}
               >
@@ -92,12 +111,26 @@ function Home() {
                   "font-size": "28px",
                 }}
               >
-                Best place to chill,study,learn and enjoy with friends.<br /> Watch
-                Youtube, Facebook videos with friends in a cool way.
+                Best place to Study and Enjoy with Friends
               </p>
+            </Grid>
+            <Grid item md={3} style={{
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+              <div className="posters" style={{
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+                <img src="gate-smashers.jpg" alt="" className="img_card_1" />
+                <img src="technical-guruji.jpg" alt="" className="img_card_1" />
+                <img src="aspirants.jpg" alt="" className="img_card_1" />
+                <img src="5-minutes.png" alt="" className="img_card_1" />
+              </div>
             </Grid>
           </Grid>
         </div>
+      <Footer />
     </div>
   );
 }
